4b5aea22c1008f9bb9937acf0aabd58d7e7e332b
Javier López de Ancos <javierlopezdeancos@gmail.com>
2017-04-29 13:20:29 +0200

    add dom library

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,68 @@
 let japp = japp;
 
 const DATA_URL = 'data.javierlopezdeancos.wedeploy.me';
+
+const PROFILES_END_POINT = '/profile';
+
+japp.profile = (function () {
+
+    const ME_NODE_ID = 'me';
+    const CONTACT_NODE_ID = 'contact';
+
+    function renderMe(member) {
+        const template = `
+            <figure class="me_figure">
+                <img class="me_pic" src="images/me.png" alt=${member.name + member.surname}/>
+                <figcaption class="me_figcaption">${member.name  + member.surname}
+                    <span class="me_job">${member.description}</span>
+                </figcaption>
+            </figure>`;
+
+        japp.dom(`#${ME_NODE_ID}`).render(template);
+    }
+
+    function renderContact(contact) {
+        return  `
+          <li class="contact_social">
+            <a href=${contact.social_networks.profile}>
+              <img src="images/social_${contact.social_networks.network}.svg" alt=${contact.social_networks.network} />
+            </a>
+          </li>`;
+    }
+
+    function renderContacts(contacts) {
+        let contactsRendered = '';
+        let template = '';
+
+        for (let contact; contact <= contacts.length; contact++) {
+            contactsRendered += renderContact(contacts[contact]);
+        }
+
+        template = `
+            <ul class="contact_socials">
+                ${contactsRendered}
+            </ul>`;
+
+        japp.dom(`#${CONTACT_NODE_ID}`).render(template);
+    }
+
+    function render(profile) {
+        renderMe();
+        renderContact();
+    }
+
+    function initialize() {
+        return WeDeploy.data(DATA_URL)
+            .get(PROFILE_END_POINT)
+            .then(function(profile) {
+                render(profile);
+            });
+    }
+
+    return {
+        initialize: initialize,
+    };
+
+})();
+
+japp.profile.initialize();
