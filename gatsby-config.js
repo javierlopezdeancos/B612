861c2063cc2d370bd807183718a9a1829c86c00c
Javier López de Ancos <javierlopezdeancos@outlook.com>
2020-10-29 20:20:30 +0100

    fix: fix trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,7 @@ const plugins = [
     resolve: `gatsby-plugin-google-analytics`,
     options: {
       // The property ID; the tracking code won't be generated without it
-      trackingId: "250321370",
+      trackingId: "2125834991",
       // Defines where to place the tracking script - `true` in the head and `false` in the body
       head: false,
       // Setting this parameter is optional
